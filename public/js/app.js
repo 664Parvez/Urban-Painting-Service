4bc82b55c78558d81ead1a85cc88aca90815b4eb
664Parvez <parvezrabbi664@gmail.com>
2025-12-11 15:06:04 +0600

    update3

--- public/js/app.js
+++ public/js/app.js
@@ -35,28 +35,6 @@ function singleProductImg(image){
     document.querySelector('.main').src = image;
 }
 
-// // Get allValues using querySelectorAll
-// let allValues = document.querySelectorAll(".value");
-
-// // Start the forEach loop for displaying the values
-// allValues.forEach((singleValue) => {
-//   let startValue = 0;
-//   let endValue = parseInt(singleValue.getAttribute("data-value"));
-//   let duration = Math.floor(2000 / endValue);
-
-//   // Counter for increaing the values & display
-//   let counter = setInterval(function () {
-//     startValue += 1;
-//     singleValue.textContent = startValue;
-//     // Clearing the interval
-//     if (startValue == endValue) {
-//       clearInterval(counter);
-//     }
-//   }, duration);
-// });
-
-
-
 const items = document.querySelectorAll(".value");
 
 gsap.from(items, {
